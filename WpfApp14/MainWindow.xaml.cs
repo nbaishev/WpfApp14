37eb4f1e1c8b99bda0ce7e1343f32bbd1168051a
Николай Баишев <baishev.nk@stud.s-vfu.ru>
2022-05-08 12:35:19 +0900

    Добавьте файлы проекта.

---
+++ WpfApp14/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProductName = "Яблоко",
+                Price = 200,
+                ProductImage = "Data/apple.jpg",
+                ProductCategory = ProductCategories.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Банан",
+                Price = 400,
+                ProductImage = "Data/banana.jpg",
+                ProductCategory = ProductCategories.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Стиральная машина",
+                Price = 20000,
+                ProductImage = "Data/washmachine.jpg",
+                ProductCategory = ProductCategories.Appliances
+            });
+            lstBox.ItemsSource = products;
+        }
+    }
+}
---
+++ WpfApp14/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp14
+{
+    public enum ProductCategories
+    {
+        Food,
+        Appliances
+        /*"Еда",
+        "Бытовая техника"*/
+    }
+
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public string ProductImage { get; set; }
+        public ProductCategories ProductCategory { get; set; }
+        public override string ToString()
+        {
+            return ProductName;
+        }
+    }
+}
